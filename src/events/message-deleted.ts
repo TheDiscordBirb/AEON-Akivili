764c1e1a08733256537b4a2981fb1dd8aad7713d
Birb <bluevidray.stream@gmail.com>
2024-07-12 01:21:25 +0200

    Instantaneous deletion from network channels

--- src/events/message-deleted.ts
+++ src/events/message-deleted.ts
@@ -9,23 +9,23 @@ const logger = new Logger("MessageDeleted");
 export default new Event("messageDelete", async (interaction) => {
     if (!interaction.webhookId) return;
     const referencedMessages = await databaseManager.getMessages(interaction.channelId, interaction.id, true);
-    for await (const referencedMessage of referencedMessages) {
+    const webhookMessages = await Promise.allSettled(referencedMessages.map(async (referencedMessage) => {
         const channel = client.channels.cache.get(referencedMessage.channelId);
         if (!channel) {
             logger.warn('Could not find channel while trying to delete message.');
-            continue;
+            return undefined;
         }
         await (channel as TextChannel).messages.fetch();
         const message = (channel as TextChannel).messages.cache.get(referencedMessage.channelMessageId);
         
         if (!message) {
-            continue;
+            return undefined;
         }
-
+    
         try {
             await message.delete();
         } catch (error) {
             logger.error('Could not delete message.', error as Error);
         }
-    }
+    }));
 });
